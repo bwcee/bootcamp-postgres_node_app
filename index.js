04d6fefec7c994d27849ba5556425b732450ce72
bwcee <bwceemail@gmail.com>
2021-11-10 15:25:23 +0800

    set-up

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 import express from "express";
 import pg from "pg";
+import methodOverride from "method-override";
 
 const { Pool } = pg;
 
 const pgConfig = {
   user: "bw_unix",
   host: "localhost",
-  database: "bw_unix",
+  database: "birding",
   port: 5432,
 };
 
 const pool = new Pool(pgConfig);
 const app = express();
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 
 app.get("/", (request, response) => {
   console.log("request came in");
