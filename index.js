199a770443f7f2b51cdff3059cff02368fc4836a
bwcee <bwceemail@gmail.com>
2021-11-10 10:12:03 +0800

    express

--- index.js
+++ index.js
@@ -1,41 +1,30 @@
+import express from "express";
 import pg from "pg";
 
-const { Client } = pg;
+const { Pool } = pg;
 
-const config = {
+const pgConfig = {
   user: "bw_unix",
   host: "localhost",
   database: "bw_unix",
   port: 5432,
 };
 
-const client = new Client(config);
+const pool = new Pool(pgConfig);
+const app = express();
 
-client.connect();
-
-const whenQueryDone = (err, result) => {
-  if (err) {
-    console.log("error", err);
-  } else {
-    console.log(result.rows);
-  }
-  client.end();
-};
-
-const entry = [
-  process.argv[3],
-  process.argv[4],
-  process.argv[5],
-  process.argv[6],
-];
-
-const sqlQuery = `INSERT INTO meals (type, description, amount_of_alcohol, was_hungry_before_eating) VALUES ('${entry[0]}', '${entry[1]}', ${entry[2]}, ${entry[3]})`;
-
-const sqlReport = `SELECT * FROM meals`;
-
-const functionality = process.argv[2];
-if (functionality === "log") {
-  client.query(sqlQuery, whenQueryDone);
-} else {
-  client.query(sqlReport, whenQueryDone);
-}
+app.get("/", (request, response) => {
+  console.log("request came in");
+  const whenDoneWithQuery = (error, result) => {
+    if (error) {
+      console.log("Error executing query", error.stack);
+      response.status(503).send(result.rows);
+      return;
+    }
+    console.log(result.rows[0].name);
+    response.send(result.rows);
+  };
+  // Query using pg.Pool instead of pg.Client
+  pool.query("SELECT * from cats", whenDoneWithQuery);
+});
+app.listen(3004);
