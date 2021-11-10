0ae0ae2d2be83d2beddb91e4e05dc6f0bd781546
bwcee <bwceemail@gmail.com>
2021-11-10 19:10:42 +0800

    comfortable

--- index.js
+++ index.js
@@ -17,18 +17,92 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
 
-app.get("/", (request, response) => {
-  console.log("request came in");
-  const whenDoneWithQuery = (error, result) => {
-    if (error) {
-      console.log("Error executing query", error.stack);
-      response.status(503).send(result.rows);
+// Show Home page
+const goHome = (request, response) => {
+  const allQuery = `SELECT * FROM notes`;
+  pool.query(allQuery, (allQueryErr, allQueryResult) => {
+    if (allQueryErr) {
+      console.error("Error", allQueryErr);
       return;
     }
-    console.log(result.rows[0].name);
-    response.send(result.rows);
-  };
-  // Query using pg.Pool instead of pg.Client
-  pool.query("SELECT * from cats", whenDoneWithQuery);
-});
+    let allNotes = allQueryResult.rows;
+    response.render("pages/home", { allNotes });
+  });
+};
+
+// Create new note
+const newNote = (request, response) => {
+  response.render("pages/new_note");
+};
+
+const doNewNote = (request, response) => {
+  const newNote = request.body;
+  const newNoteQuery = `INSERT INTO notes (habitat, date, appearance, behavior, vocalisation, flock_size) VALUES ('${newNote.habitat}', '${newNote.date}', '${newNote.appearance}', '${newNote.behavior}', '${newNote.vocalisation}', '${newNote.flock_size}')`;
+  pool.query(newNoteQuery);
+  response.redirect(301, "/");
+};
+
+// Show note
+const showNote = (request, response) => {
+  const { idToShow } = request.params;
+  const noteQuery = `SELECT * FROM notes WHERE id=${idToShow}`;
+  pool.query(noteQuery, (noteQueryErr, noteQueryResult) => {
+    if (noteQueryErr) {
+      console.error("Error", noteQueryErr);
+      return;
+    }
+    const noteContents = noteQueryResult.rows[0];
+    response.render("pages/full_note", { noteContents });
+  });
+};
+
+// Delete note
+const deleteNote = (request, response) => {
+  const { idToDelete } = request.params;
+  const deleteQuery = `DELETE FROM notes WHERE id=${idToDelete}`;
+  pool.query(deleteQuery, (deleteQueryErr, deleteQueryResult) => {
+    if (deleteQueryErr) {
+      console.error("Error", deleteQueryErr);
+      return;
+    }
+    response.redirect(301, "/");
+  });
+};
+
+// Edit note
+const editNote = (request, response) => {
+  const { idToEdit } = request.params;
+  const editQuery = `SELECT * FROM notes WHERE id=${idToEdit}`;
+  pool.query(editQuery, (editQueryErr, editQueryResult) => {
+    if (editQueryErr) {
+      console.error("Error", editQueryErr);
+      return;
+    }
+    const noteContents = editQueryResult.rows[0];
+    response.render("pages/edit", { noteContents });
+  });
+};
+
+const doEditNote = (request, response) => {
+  const editedNote = request.body;
+  const { idToEdit } = request.params;
+  console.log(editedNote)
+  const doEditQuery = `UPDATE notes SET habitat= '${editedNote.habitat}', date= '${editedNote.date}', appearance= '${editedNote.appearance}', behavior= '${editedNote.behavior}', vocalisation= '${editedNote.vocalisation}', flock_size= '${editedNote.flock_size}'  WHERE id=${idToEdit}`;
+  pool.query(doEditQuery, (doEditQueryErr, doEditQueryResult) => {
+    if (doEditQueryErr) {
+      console.error("Error", doEditQueryErr);
+      return;
+    }
+    response.redirect(301, '/')
+  });
+};
+
+app.get("/note", newNote);
+app.post("/note", doNewNote);
+app.get("/note/:idToShow", showNote);
+app.get("/", goHome);
+app.delete("/note/delete/:idToDelete", deleteNote);
+app.get("/note/edit/:idToEdit", editNote);
+app.put("/note/edit/:idToEdit", doEditNote);
+
 app.listen(3004);
